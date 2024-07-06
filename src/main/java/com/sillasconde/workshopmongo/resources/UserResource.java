9d9f3f4f045eb255aa642d203ba112795052d129
sillasconde <sillasconde@usp.br>
2024-07-06 17:07:43 -0300

    Get user posts endpoint

--- src/main/java/com/sillasconde/workshopmongo/resources/UserResource.java
+++ src/main/java/com/sillasconde/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.sillasconde.workshopmongo.domain.Post;
 import com.sillasconde.workshopmongo.domain.User;
 import com.sillasconde.workshopmongo.dto.UserDTO;
 import com.sillasconde.workshopmongo.services.UserService;
@@ -77,6 +78,16 @@ public class UserResource {
 		service.update(obj);
 		// created: 201 code
 		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
+		
 	}
 
 }
